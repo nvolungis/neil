4c23febf6b25b3ea09cd89e00488b2c09855adaf
Neil Volungis <nvolungis@Neil-MacBookPro.(none)>
2014-05-21 16:35:05 -0400

    adding my updated resume

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,5 +1,5 @@
 class FilesController < ApplicationController
   def resume
-    redirect_to 'http://neilvolungis.s3.amazonaws.com/neil-volungis-res.pdf'
+    redirect_to 'http://neilvolungis.s3.amazonaws.com/neil-volungis-resume.pdf'
   end
 end
