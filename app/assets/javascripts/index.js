c9a14da574ab83fc11c1cc0bafcc20236f97e0ea
Neil Volungis <nvolungis@Neil-MacBookPro.(none)>
2014-06-04 01:43:25 -0400

    responsive!!

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -9,9 +9,24 @@
     if(!Modernizr.touch) skrollr.init({
       forceHeight: false
     });
+
+    enquire.register('screen and (min-width:700px)', {
+      match: function(){
+        register_chapters();
+      }, 
+
+      unmatch: function(){
+        unregister_chapters();
+      },
+
+      setup: function(){
+        setup_chapters();
+      }
+    });
+
   });
 
-  register_chapters();
+
 
   $(window).trigger('scroll');
 }(jQuery));
--- app/assets/javascripts/lib/chapters.js
+++ app/assets/javascripts/lib/chapters.js
@@ -1,200 +1,196 @@
 (function($){
-  var controller, chapters, register_chapters;
+  var controller, chapters, register_chapters, scenes = [];
+
+  function update_status(inview, $el){
+    if(inview){
+      $el.addClass('active');
+    }else {
+      $el.removeClass('active');
+    }
+  }
+
+  function activate($el){
+    $el.addClass('active');
+  }
 
   controller = new ScrollMagic();
 
-  chapters = [{
-    //Chapter 1;
-
+  scenes.push({
     register: function(){
-      var tween, scene, scene_actions, $sketch;
-      
-      $sketch = $('.fts-introduction-figure');
-      tween = TweenMax.to( '.fts-introduction-figure', .5, {y: -250});
-      scene = new ScrollScene({
-        triggerElement: '.chapter-1',
-        triggerHook: 0,
-        duration: 1000
-      })
-      .setTween(tween)
-      .addTo(controller);
+      this.$process_icons = $('.process-icons');
+      this.$chapter     = $('.chapter-3');
 
-      scene_actions = new SceneActions(scene, {
+      this.$process_icons.bind('inview', function(e, inview, vx, vy){
+        update_status(inview, this.$chapter);
+      }.bind(this));
+    },
+
+    unregister: function(){
+      this.$process_icons.unbind('inview');
+      activate(this.$chapter);
+    },
+
+    setup: function(){
+      var $process_icons = $('.process-icons');
+
+      activate($process_icons);
+    }
+  });
+  
+  scenes.push({
+    register: function(){
+      this.$heart = $('.heart');
+
+      this.$heart.on('inview', function(e, inview){
+        update_status(inview, this.$heart);
+      }.bind(this));
+    },
+
+    unregister: function(){
+      this.$heart.off('inview');
+      this.$heart.removeClass('active');
+    },
+    
+    setup: function(){
+      var $heart = $('.heart');
+      $heart.removeClass('active');
+    }
+  });
+
+  scenes.push({
+    register: function(){
+      this.$sentence = $('.sentence');
+      this.$chapter = $('.chapter-5');
+      
+      this.$sentence.bind('inview', function(e, inview) {
+        update_status(inview, this.$chapter);
+      }.bind(this));
+    },
+
+    unregister: function(){
+      this.$sentence.unbind('inview');
+      activate(this.$chapter);
+    },
+
+    setup: function(){
+      var $chapter = $('.chapter-5');
+      $chapter.addClass('active');
+    }
+  })
+
+  // The responsive design scene
+  scenes.push({
+    register: function(){
+      var scene, scene_actions, scene2, scene2_actions, $chapter, $responsive_animation,
+          that = this;
+
+      this.$chapter = $('.chapter-2');
+      this.$responsive_animation = $('.responsive-animation');
+
+      this.$chapter.css({
+        height: '100%'
+      });
+      
+      this.scene = new ScrollScene({
+         triggerElement: '.chapter-2', 
+         triggerHook: 0,
+         duration: 1000
+       }).setPin('.chapter-2').addTo(controller);
+
+      scene_actions = new SceneActions(this.scene, {
+        internal_actions:[{
+          start: 0, end: .3,
+          onStart: function(){
+            that.$responsive_animation.addClass('desktop').removeClass('tablet mobile');
+            that.$chapter.addClass('active');
+          }.bind(that),
+          onEnd: function(){ }
+        },
+        {
+          start: .31, end: .6,
+          onStart: function(){
+            that.$responsive_animation.addClass('tablet').removeClass('desktop mobile');
+            that.$chapter.addClass('active');
+          }.bind(that),
+          onEnd: function(){ }
+        },
+        {
+          start: .61, end: 1,
+          onStart: function(){
+            that.$responsive_animation.addClass('mobile').removeClass('desktop tablet');
+            that.$chapter.addClass('active');
+          }.bind(that),
+          onEnd: function(){ }
+        }]
+      });
+
+      this.scene2 = new ScrollScene({
+         triggerElement: '.responsive-animation',
+         duration: 600
+       }).addTo(controller);
+
+      scene2_actions = new SceneActions(this.scene2, {
+        on_before:function(){
+          that.$chapter.removeClass('active');
+        },
+
         on_inside: function(){
-          $sketch.addClass('active');
+          that.$chapter.addClass('active');
         },
 
         on_after: function(){
-          $sketch.removeClass('active');
+          that.$chapter.addClass('active');
+          that.$responsive_animation.addClass('mobile').removeClass('desktop tablet');
         }
       });
-
-    }},
-
-    {
-      //Chapter 2
-      register: function(){
-        var scene, scene_actions, scene2, scene2_actions, $chapter, $responsive_animation;
-
-        $chapter = $('.chapter-2');
-        $responsive_animation = $('.responsive-animation');
-        
-        scene = new ScrollScene({
-           triggerElement: '.chapter-2', 
-           triggerHook: 0,
-           duration: 1000
-         }).setPin('.chapter-2').addTo(controller);
-
-        scene_actions = new SceneActions(scene, {
-          internal_actions:[{
-            start: 0, end: .3,
-            onStart: function(){
-              $responsive_animation.addClass('desktop').removeClass('tablet mobile');
-              $chapter.addClass('active');
-            },
-            onEnd: function(){ }
-          },
-          {
-            start: .31, end: .6,
-            onStart: function(){
-              $responsive_animation.addClass('tablet').removeClass('desktop mobile');
-              $chapter.addClass('active');
-            },
-            onEnd: function(){ }
-          },
-          {
-            start: .61, end: 1,
-            onStart: function(){
-              $responsive_animation.addClass('mobile').removeClass('desktop tablet');
-              $chapter.addClass('active');
-            },
-            onEnd: function(){ }
-          }]
-        });
-
-        scene2 = new ScrollScene({
-           triggerElement: '.responsive-animation',
-           duration: 600
-         }).addTo(controller);
-
-        scene2_actions = new SceneActions(scene2, {
-          on_before:function(){
-            $chapter.removeClass('active');
-          },
-
-          on_inside: function(){
-            $chapter.addClass('active');
-          },
-
-          on_after: function(){
-            $chapter.addClass('active');
-            $responsive_animation.addClass('mobile').removeClass('desktop tablet');
-          }
-        });
-      }
     },
 
-    {
-      // Chapter 3
-      register: function(){
-        var tween, scene, scene_actions, scene2, scene2_actions, $chapter;
+    unregister: function(){
+      var $spacer = $('.scrollmagic-pin-spacer'),
+          $chapter_2 = $spacer.find('.chapter-2').clone(),
+          $before_spacer = $spacer.prev();
 
-        $chapter = $('.chapter-3');
-        var $chapter2 = $('.chapter-2');
+      $spacer.remove();
+      $chapter_2.find('.responsive-animation').removeClass('tablet mobile').addClass('desktop');
+      $chapter_2.insertAfter($before_spacer).css({
+        position: 'static',
+        height: 'auto'
+      }).addClass('active');
 
-        // tween = new TweenMax.to('.chapter-2', .5, {y: 400});
-
-        // scene = new ScrollScene({
-        //   triggerElement: '.chapter-3',
-        //   triggerHook: 1,
-        //   duration: 1000
-        // }).addTo(controller);
-
-        scene2 = new ScrollScene({
-          triggerElement: '.process-icons',
-          duration: 100
-        }).addTo(controller);
-
-        scene2_actions = new SceneActions(scene2, {
-          on_before:function(){
-            $chapter.removeClass('active');
-          },
-
-          on_inside: function(){
-            $chapter.addClass('active');
-          },
-
-          on_after: function(){
-            $chapter.addClass('active');
-          }
-        });
-      }
+      this.scene.destroy();
+      this.scene2.destroy();
     },
 
-    {
-      // Chapter 4
-      register: function(){
-        var scene, scene_actions, $heart;
-
-        $heart = $('.heart');
-
-        scene = new ScrollScene({
-          triggerElement: '.heart',
-          duration: 100
-        }).addTo(controller);
-
-        scene_actions = new SceneActions(scene, {
-          on_before:function(){
-            $heart.removeClass('active');
-          },
-
-          on_inside: function(){
-            $heart.addClass('active');
-          },
-
-          on_after: function(){
-            $heart.addClass('active');
-          }
-        });
-      }
-    },
-
-    {
-      // Chapter 5
-      register: function(){
-        var scene, scene_actions, $chapter;
-
-        $chapter = $('.chapter-5');
-
-        scene = new ScrollScene({
-          triggerElement: '.sentence',
-          duration: 100
-        }).addTo(controller);
-
-        scene_actions = new SceneActions(scene, {
-          on_before:function(){
-            $chapter.removeClass('active');
-          },
-
-          on_inside: function(){
-            $chapter.addClass('active');
-          },
-
-          on_after: function(){
-            $chapter.addClass('active');
-          }
-        });
-      }
-    }];
-
-    register_chapters = function(){
-      for(var i = 0; i < chapters.length; i += 1){
-        // chapters[i].register(); 
-      }
-
-      chapters[1].register();
+    setup: function(){
+      console.log('setup');
+      $('.chapter-2').addClass('active');
+      $('.responsive-animation').find('.responsive-animation').removeClass('tablet mobile').addClass('desktop');
     }
+  });
 
-    window.register_chapters = register_chapters;
+  setup_chapters = function(){
+    console.log('setup_chapters');
+    for(var i = 0; i < scenes.length; i += 1){
+      if(scenes[i].setup){
+        scenes[i].setup(); 
+      }
+    }
+  }
+
+  register_chapters = function(){
+    for(var i = 0; i < scenes.length; i += 1){
+      scenes[i].register(); 
+    }
+  }
+
+  unregister_chapters = function(){
+    for(var i = 0; i < scenes.length; i += 1){
+      scenes[i].unregister(); 
+    }
+  }
+
+  window.register_chapters = register_chapters;
+  window.unregister_chapters = unregister_chapters;
+  window.setyps_chapters = setup_chapters;
 
 }(jQuery));
